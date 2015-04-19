d55a8b664ceeffd6c57aa7c56cf166de414deee0
robbindn <daniel_robbins@merck.com>
2015-04-19 00:01:52 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to efficiently invert a matrix
+##   For the first execution of the function, the calculation will be done.
+##   For subsequent executions, a cached value for the matrix will be used.  To
+##   handle the cacheing, lexical scoping is taken advantage of.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix constructs a list of functions.  The functions are as follows:
+##    set - takes a matrix and saves it to the parent environment (cacheSolve)
+##    get - retrieves the original matrix
+##    setInverse - saves the inverted matrix to the parent environment (cacheSolve)
+##    getInverse - retrieves the inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
   InvMatrix <- NULL
   set <- function(y) {
@@ -19,17 +24,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## List created with makeCacheMatrix is passed to this function.  If the inverted
+###   matrix is already calculated, that value is returned.  If it is not, the 
+###   solve function is used to calculate it.  Functions in makeCacheMatrix are used
+###   to get and retrieve the matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         InvMatrix <- x$getInverse()
-        message("Step 1")
         if(!is.null(InvMatrix)) {
               message("getting cached data")
               return(InvMatrix)
         }
-        message("Step 2")
         OrigMatrix <- x$get()
         InvMatrix <- solve(OrigMatrix)
         x$setInverse(InvMatrix)
